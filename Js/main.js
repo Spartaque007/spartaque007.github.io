12abc535e4c402e6221b9d87c4884e59e7eeefdb
SPARTAQUE <spartaque007@gmail.com>
2022-02-22 00:19:35 +0300

    done

---
+++ Js/main.js
@@ -0,0 +1,21 @@
+
+// document.addEventListener('DOMContentLoaded', onDocumentReady);
+
+
+
+//  function onDocumentReady()
+//  {
+//     var mydata = JSON.parse(data)
+
+//     initData();
+//     document.getElementById("body").innerHTML = `${mydata}`;
+
+
+//  }
+
+//  function initData()
+//  {
+    
+  
+
+//  }
